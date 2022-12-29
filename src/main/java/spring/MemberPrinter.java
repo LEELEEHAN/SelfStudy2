07d07bd0f910c63dc4b2a71b5eab0939c22ce817
iee <horocolow@gmail.com>
2022-12-29 23:50:01 +0900

    1228

--- src/main/java/spring/MemberPrinter.java
+++ src/main/java/spring/MemberPrinter.java
@@ -1,11 +1,28 @@
 package spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.format.DateTimeFormatter;
+
 public class MemberPrinter {
 
+    private DateTimeFormatter dateTimeFormatter;
     public void print(Member member){
-        System.out.printf(
-                "id=%d, email=%s, name=%s, date=%tF\n",
-                member.getId(),member.getEmail(),
-                member.getName(),member.getRegisterDateTime());
+        if(dateTimeFormatter ==null){
+            System.out.printf(
+                    "id=%d, email=%s, name=%s, date=%tF\n",
+                    member.getId(),member.getEmail(),
+                    member.getName(),member.getRegisterDateTime());
+        } else {
+            System.out.printf(
+                    "회원정보 id =%d, email = %s, name = %s, date=%s",
+                    member.getId(),member.getEmail(),member.getName(),
+                    dateTimeFormatter.format(member.getRegisterDateTime()));
+            
+        }
+    }
+    @Autowired
+    public void setDateTimeFormatter(DateTimeFormatter dateTimeFormatter){
+        this.dateTimeFormatter =dateTimeFormatter;
     }
 }
